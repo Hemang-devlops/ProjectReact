45665e097d78263b5d51a0d1caa9f505653da8ab
darshakgadher <gadherdarshak1307@gmail.com>
2023-09-12 21:07:18 +0530

    video player

--- demo/src/components/paths/Introduction.js
+++ demo/src/components/paths/Introduction.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import NavBar from "../NavBar/NavBar";
+import VideoPlayer from "../VideoPlayer/VideoPlayer";
 
 export default function Introduction({ data }) {
+  const [style, setStyle] = useState({
+    width: "100%",
+    height: "90vh",
+  });
   return (
     <div>
       <NavBar data={data} value={"Introduction"} />
+      <div>
+        <VideoPlayer
+          url="https://www.youtube.com/watch?v=LXb3EKWsInQ"
+          width={style.width}
+          height={style.height}
+        />
+      </div>
     </div>
   );
 }
